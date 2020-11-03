b83d262564ba9de1c66a8fb2dcbcb4f96cbd155d
Derek Vu <beetee@Dereks-MacBook-Pro.local>
2020-11-02 21:32:24 -0600

    update the API endpoint

--- src/App.js
+++ src/App.js
@@ -78,11 +78,7 @@ class App extends Component {
 
   addItem = (item, id) =>  {
     if (item.todo !== ""){
-<<<<<<< HEAD
       fetch('https://frozen-river-89593.herokuapp.com/add/' + id, {
-=======
-      fetch('https://frozen-river-89593.herokuapp.com/add' + id, {
->>>>>>> 172c85dd6ecf157ea8f551c8686ea929b52dd661
         method:'post',
         headers:{'Content-Type' : 'application/json'},
         body:JSON.stringify({
@@ -103,11 +99,7 @@ class App extends Component {
 
   editItem = (todo, id)=>{
     
-<<<<<<< HEAD
     fetch('https://frozen-river-89593.herokuapp.com/edit/' + id, {
-=======
-    fetch('https://frozen-river-89593.herokuapp.com/edit' + id, {
->>>>>>> 172c85dd6ecf157ea8f551c8686ea929b52dd661
         method:'PUT',
         headers:{'Content-Type' : 'application/json', 'Authorization' : 'sdfsfsfwiiowuerewrwrewrww'},
         body:JSON.stringify({
@@ -127,11 +119,7 @@ class App extends Component {
   }
 
   deleteItem = (key) =>{
-<<<<<<< HEAD
     fetch('https://frozen-river-89593.herokuapp.com/delete/' + key, {
-=======
-    fetch('https://frozen-river-89593.herokuapp.com/delete' + key, {
->>>>>>> 172c85dd6ecf157ea8f551c8686ea929b52dd661
       method:'delete',
       headers:{'Content-Type' : 'application/json'},
       body:JSON.stringify({ 
@@ -149,11 +137,7 @@ class App extends Component {
     console.log(item)
 
     if (item.done != 0){
-<<<<<<< HEAD
       fetch('https://frozen-river-89593.herokuapp.com/toggle/' + item.noteid, {
-=======
-      fetch('https://frozen-river-89593.herokuapp.com/toggle' + item.noteid, {
->>>>>>> 172c85dd6ecf157ea8f551c8686ea929b52dd661
         method:'PUT',
         headers:{'Content-Type' : 'application/json', 'Authorization' : 'sdfsfsfwiiowuerewrwrewrww'},
         body:JSON.stringify({
@@ -170,11 +154,7 @@ class App extends Component {
         this.setState({items:editedItems})
       }) 
     } else {
-<<<<<<< HEAD
       fetch('https://frozen-river-89593.herokuapp.com/toggle/' + item.noteid, {
-=======
-      fetch('https://frozen-river-89593.herokuapp.com/toggle' + item.noteid, {
->>>>>>> 172c85dd6ecf157ea8f551c8686ea929b52dd661
         method:'PUT',
         headers:{'Content-Type' : 'application/json', 'Authorization' : 'sdfsfsfwiiowuerewrwrewrww'},
         body:JSON.stringify({
